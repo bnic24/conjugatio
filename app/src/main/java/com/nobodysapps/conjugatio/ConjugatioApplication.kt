30a9fe24fdeef4abbcd128b2de4acee519c351ac
Jonathan Loos <loos.jonathan.martin@gmail.com>
2022-07-26 00:56:56 +0200

    Add ConjugatioApplication.kt for using hilt

---
+++ app/src/main/java/com/nobodysapps/conjugatio/ConjugatioApplication.kt
@@ -0,0 +1,8 @@
+package com.nobodysapps.conjugatio
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ConjugatioApplication : Application() {
+}
--- app/src/main/java/com/nobodysapps/conjugatio/MainActivity.kt
+++ app/src/main/java/com/nobodysapps/conjugatio/MainActivity.kt
@@ -3,15 +3,13 @@ package com.nobodysapps.conjugatio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.nobodysapps.conjugatio.navigation.setUpNavigation
 import com.nobodysapps.conjugatio.ui.theme.ConjugatioTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
